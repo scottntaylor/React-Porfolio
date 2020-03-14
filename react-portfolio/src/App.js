cb71b417019235fd1d23b28af97f2e291d4fab70
Scott Taylor <scottntaylor21@gmail.com>
2020-03-14 13:23:17 -0700

    Navbar Setup

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/container';
+import NavBar from 'react-bootstrap/NavBar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
 
 class App extends React.Component {
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       title: "Scott Taylor",
-      headerLinks:[
-        { title: 'Home', path: '/'},
-        { title: 'About', path: '/abou'},
-        { title: 'Contact', path: '/contact'},
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/abou' },
+        { title: 'Contact', path: '/contact' },
 
       ],
       home: {
         title: 'To Be Determined',
         subtitle: 'To Be Determined',
-        body: 'To Be Determined' 
+        body: 'To Be Determined'
       },
       about: {
         title: 'About Me'
@@ -26,16 +29,34 @@ class App extends React.Component {
         title: 'Lets Talk!'
       }
     }
-     
+
   }
 
-  render(){
+  render() {
 
     return (
-   <div>Hello from React!</div>
-      );
+
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <NavBar className="border-bottom" bg="trans parent" expand="lg">
+            <NavBar.Brand>Scott Taylor</NavBar.Brand>
+
+
+            <NavBar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <NavBar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home </Link>
+                <Link className="nav-link" to="/about">About </Link>
+                <Link className="nav-link" to="/contact">Contact </Link>
+              </Nav>
+            </NavBar.Collapse>
+          </NavBar>
+        </Container>
+
+      </Router>
+    )
   }
-  
+
 }
 
 export default App;
